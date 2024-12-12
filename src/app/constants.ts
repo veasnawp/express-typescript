e336902511126469faa4e263b9d111c0e39c047e
Veasna WP <veasnawp@gmail.com>
2024-12-13 05:23:20 +0700

    update constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 export const env = process.env;
-export const IS_DEV = env.NODE_ENV !== "development";
+export const IS_DEV = env.NODE_ENV !== "production";
 
 export const PORT = env.PORT || 3000;
 export const CLIENT_URL = env.CLIENT_URL || `http://localhost:${PORT}`;
